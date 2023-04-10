b0f04c38a09b9427490640d6fd3676261e5b375c
cguzman18 <37959481+cguzman18@users.noreply.github.com>
2023-04-09 20:10:03 -0400

    Updated code. Started testing

--- StarCraft_Bot.py
+++ StarCraft_Bot.py
@@ -1,85 +1,87 @@
 import sc2
-from sc2 import run_game, maps, Race, Difficulty
+from sc2 import run_game, maps, Race, Difficulty, Result
 from sc2.player import Bot, Computer
 from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR, GATEWAY, CYBERNETICSCORE, STALKER, STARGATE, VOIDRAY
 import random
 
-# TODO: Come of with a cool name for our bot
-class StarCraftBot(sc2.BotAI):
-    def __init__(self):
+
+class SentdeBot(sc2.BotAI):
+    def __init__(self, MAX_STALKER, MAX_VOIDRAY):
         self.ITERATIONS_PER_MINUTE = 165
         self.MAX_WORKERS = 50
+        self.MAX_STALKER = MAX_STALKER
+        self.MAX_VOIDRAY = MAX_VOIDRAY
+
 
     async def on_step(self, iteration):
-        # NOTE: This section tells you what to do at each step
         self.iteration = iteration
-        await self.distribute_workers() # defined in sc2/bot_ai.py; makes workers collect resources
-        await self.build_workers() # build more workers if resources permit
-        await self.build_pylons() # build more pylons if resources permit
-        await self.build_assimilator() # structur to collect the gas resource
-        await self.expand() # expand to new resources area
-        await self.build_offensive_buildings() # build offensive buildings
-        await self.build_offensive_forces() # build offensive forces
-        await self.attack() # attack enemy with build offensive forces
+
+        await self.distribute_workers()
+        await self.build_workers()
+        await self.build_pylons()
+        await self.build_assimilators()
+        await self.expand()
+        await self.offensive_force_buildings()
+        await self.build_offensive_force()
+        await self.attack()
 
     async def build_workers(self):
-        # NOTE: Nexus is the command ceter and Probe are the workers; there can be more than 1 Nexus
         if (len(self.units(NEXUS)) * 16) > len(self.units(PROBE)) and len(self.units(PROBE)) < self.MAX_WORKERS:
             for nexus in self.units(NEXUS).ready.idle:
                 if self.can_afford(PROBE):
                     await self.do(nexus.train(PROBE))
 
+
     async def build_pylons(self):
-        # NOTE: Pylons increases the amount of units you are allowed to have
         if self.supply_left < 5 and not self.already_pending(PYLON):
             nexuses = self.units(NEXUS).ready
             if nexuses.exists:
                 if self.can_afford(PYLON):
                     await self.build(PYLON, near=nexuses.first)
-    
-    async def build_assimilator(self):
-        # NOTE: only building assimilator thats within 25 units radius of a Nexus
+
+    async def build_assimilators(self):
         for nexus in self.units(NEXUS).ready:
-            vaspenes = self.state.vespene_geyser.closer_than(25.0, nexus)
+            vaspenes = self.state.vespene_geyser.closer_than(15.0, nexus)
             for vaspene in vaspenes:
                 if not self.can_afford(ASSIMILATOR):
-                    break # If you cant afford dont build
+                    break
                 worker = self.select_build_worker(vaspene.position)
                 if worker is None:
-                    break # If no worker is available dont build
+                    break
                 if not self.units(ASSIMILATOR).closer_than(1.0, vaspene).exists:
                     await self.do(worker.build(ASSIMILATOR, vaspene))
-    
+
     async def expand(self):
-        # NOTE: only building 2 Nexus, may want to build more
-        if self.units(NEXUS).amount < 2 and self.can_afford(NEXUS):
+        if self.units(NEXUS).amount < (self.iteration / self.ITERATIONS_PER_MINUTE) and self.can_afford(NEXUS):
             await self.expand_now()
 
-    async def build_offensive_buildings(self):
+    async def offensive_force_buildings(self):
+        #print(self.iteration / self.ITERATIONS_PER_MINUTE)
         if self.units(PYLON).ready.exists:
             pylon = self.units(PYLON).ready.random
-            
+
             if self.units(GATEWAY).ready.exists and not self.units(CYBERNETICSCORE):
-                    if self.can_afford(CYBERNETICSCORE) and not self.already_pending(CYBERNETICSCORE):
-                        await self.build(CYBERNETICSCORE, near=pylon)
-            elif len(self.units(GATEWAY)) < ((self.iteration / self.ITERATIONS_PER_MINUTE)/2):
+                if self.can_afford(CYBERNETICSCORE) and not self.already_pending(CYBERNETICSCORE):
+                    await self.build(CYBERNETICSCORE, near=pylon)
+
+            elif len(self.units(GATEWAY)) < ((self.iteration / self.ITERATIONS_PER_MINUTE)):
                 if self.can_afford(GATEWAY) and not self.already_pending(GATEWAY):
                     await self.build(GATEWAY, near=pylon)
 
             if self.units(CYBERNETICSCORE).ready.exists:
-                if len(self.units(STARGATE)) < ((self.iteration / self.ITERATIONS_PER_MINUTE)/2):
+                if len(self.units(STARGATE)) < ((self.iteration / self.ITERATIONS_PER_MINUTE)):
                     if self.can_afford(STARGATE) and not self.already_pending(STARGATE):
                         await self.build(STARGATE, near=pylon)
 
-    async def build_offensive_forces(self):
-        for gateway in self.units(GATEWAY).ready.idle:
-            if not self.units(STALKER).amount > self.units(VOIDRAY).amount:
+    async def build_offensive_force(self):
+        for gw in self.units(GATEWAY).ready.idle:
+            if not self.units(STALKER).amount > self.units(VOIDRAY).amount and self.units(CYBERNETICSCORE):
                 if self.can_afford(STALKER) and self.supply_left > 0:
-                    await self.do(gateway.train(STALKER))
-        
-        for stargate in self.units(STARGATE).ready.idle:
+                    await self.do(gw.train(STALKER))
+
+        for sg in self.units(STARGATE).ready.idle:
             if self.can_afford(VOIDRAY) and self.supply_left > 0:
-                await self.do(stargate.train(VOIDRAY))
+                await self.do(sg.train(VOIDRAY))
 
     def find_target(self, state):
         if len(self.known_enemy_units) > 0:
@@ -87,20 +89,52 @@ class StarCraftBot(sc2.BotAI):
         elif len(self.known_enemy_structures) > 0:
             return random.choice(self.known_enemy_structures)
         else:
-            return self.enemy_start_locations[0] 
-    
-    async def attack(self):
-        aggressive_units = {STALKER: [15,5], VOIDRAY: [8,3]}
+            return self.enemy_start_locations[0]
 
+    async def attack(self):
+        # {UNIT: [n to fight, n to defend]}
+        aggressive_units = {STALKER: self.MAX_STALKER,
+                            VOIDRAY: self.MAX_VOIDRAY}
+        
         for UNIT in aggressive_units:
-            if self.units(UNIT).amount > aggressive_units[UNIT][0] and self.units(UNIT).amount > aggressive_units[UNIT][1]:
+            if self.units(UNIT).amount > aggressive_units[UNIT]:
                 for s in self.units(UNIT).idle:
                     await self.do(s.attack(self.find_target(self.state)))
 
-            elif self.units(UNIT).amount > aggressive_units[UNIT][1]:
+            else:
                 if len(self.known_enemy_units) > 0:
                     for s in self.units(UNIT).idle:
                         await self.do(s.attack(random.choice(self.known_enemy_units)))
 
+max_stalker = 10
+max_voidray = 5
 
-run_game(maps.get("AbyssalReefLE"), [ Bot(Race.Protoss, StarCraftBot()), Computer(Race.Terran, Difficulty.Hard)], realtime=False)
+for i in range(5):
+    wins = 0
+    losses = 0
+
+    for j in range(10):
+        results = run_game(maps.get("AbyssalReefLE"), [ Bot(Race.Protoss, SentdeBot(max_stalker,max_voidray)), Computer(Race.Terran, Difficulty.Hard)], realtime=False)
+        if results == Result.Victory:
+            wins += 1
+        else:
+            losses += 1
+    
+    f = open("sc2_results.txt","a")
+    f.write("----- Experiment Results ------\n")
+    f.write("Max Stalker/Voidray : " + str(max_stalker) + "/" + str(max_voidray) + "\n")
+    f.write("Total Games: " + str(wins+losses) + "\n")
+    f.write("Wins: " + str(wins) + "\n")
+    f.write("Losses: " + str(losses) + "\n")
+    f.write("Difficulty: Harder\n")
+    f.write("Computer Race: Terran\n")
+    f.write("*************************\n")
+    f.close()
+
+    print ("----- Experiment Results ------")
+    print ("Total Games: " + str(wins+losses))
+    print ("Wins: " + str(wins))
+    print ("Losses: " + str(losses))
+
+    max_stalker += 2
+    max_voidray += 1
